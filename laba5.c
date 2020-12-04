2743801a2ae87247ad7fc12a04c12b1e7da81a5c
Илья Папко <71662397+hHasP@users.noreply.github.com>
2020-12-04 10:04:25 +0300

    Add files via upload

---
+++ laba5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define N 7
+
+int main()
+{
+    
+    int arr[N][N];
+    int i,j,a=0;
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            arr[i][j] = rand() % 100 - 10;
+            printf("%d\t", arr[i][j]);
+        }
+        printf("\n");
+    }
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            if ((arr[i][j] % 2 == -1) & (arr[i][j] < 0))
+            {
+                a = a +abs(arr[i][j]);
+            }
+        }
+    }
+
+        printf("%d  ", a);
+}
